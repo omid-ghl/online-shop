c29749f59e0f30634507be27568760ba2f319021
Omid <omidgholami1@yahoo.com>
2023-11-11 20:37:59 +0330

    [base] base code uploaded

---
+++ mock/slider.js
@@ -0,0 +1,23 @@
+export const sliderContent = [
+  {
+    ID: 1,
+    title: "digitalBT",
+    description: "digitalBD",
+    bgImg: "url('/images/slider-img/nuts-1.jpg')",
+    url: "/",
+  },
+  {
+    ID: 2,
+    title: "stationeryBT",
+    description: "stationeryBD",
+    bgImg: "url('/images/slider-img/nuts-2.jpg')",
+    url: "/",
+  },
+  {
+    ID: 3,
+    title: "toyBT",
+    description: "toyBD",
+    bgImg: "url('/images/slider-img/nuts-3.jpg')",
+    url: "/",
+  },
+];
