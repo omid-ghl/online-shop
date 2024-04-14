03291d59be82585eed99c6186b24009f75ab52a0
najı <naji@najs-iMac.local>
2024-04-14 09:56:59 +0330

    []

--- components/Offers/Offers.tsx
+++ components/Offers/Offers.tsx
@@ -1,20 +1,34 @@
-import React from "react";
-import CarouselBox from "../UI/CarouselBox/CarouselBox";
-import { useSelector } from "react-redux";
-import { IProduct } from "../../lib/types/products";
-import CarouselBoxCard from "../UI/CarouselBox/CarouselBoxCard";
+import { useSelector } from 'react-redux';
+import { IProduct } from '../../lib/types/products';
+import CarouselBox from '../UI/CarouselBox/CarouselBox';
+import CarouselBoxCard from '../UI/CarouselBox/CarouselBoxCard';
 
 const Offers = () => {
-  const OfferProducts = useSelector(
-    (state: any) => state.specialOfferProductsList.specialOfferProducts
-  );
+  const OfferProducts = useSelector((state: any) => state.specialOfferProductsList.specialOfferProducts);
 
   return (
-    <div className="md:mt-10 w-full xl:max-w-[2100px] mx-auto">
-      <CarouselBox title="offers" className="bg-offersBG" href="/">
-        {OfferProducts.slice(0, 10).map((product: IProduct) => {
-          return <CarouselBoxCard key={product.name} product={product} />;
-        })}
+    <div className="md:mt-10 w-full xl:max-w-[2100px] mx-auto ">
+      <CarouselBox title="offers" className="bg-offersBG flex-row" href="/">
+        {[
+          ...OfferProducts,
+          ...OfferProducts,
+          ...OfferProducts,
+          ...OfferProducts,
+          ...OfferProducts,
+          ...OfferProducts,
+          ...OfferProducts,
+          ...OfferProducts,
+          ...OfferProducts,
+          ...OfferProducts,
+        ]
+          .slice(0, 10)
+          .map((product: IProduct) => {
+            return (
+              <>
+                <CarouselBoxCard key={product.name} product={product} />
+              </>
+            );
+          })}
       </CarouselBox>
     </div>
   );
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,49 +1,42 @@
-import axios from "axios";
-import type { NextPage } from "next";
-import { useDispatch } from "react-redux";
-import { useRouter } from "next/router";
-import jsCookie from "js-cookie";
-import EnteringBox from "../components/entering/EnteringBox";
-import { IUser, IUserInfoRootState } from "../lib/types/user";
-import { userInfoActions } from "../store/user-slice";
-import { getError } from "../utilities/error";
-import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import axios from 'axios';
+import jsCookie from 'js-cookie';
+import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import EnteringBox from '../components/entering/EnteringBox';
+import { IUser, IUserInfoRootState } from '../lib/types/user';
+import { userInfoActions } from '../store/user-slice';
+import { getError } from '../utilities/error';
 const Login: NextPage = () => {
   const dispatch = useDispatch();
   const router = useRouter();
-  const [errorMessage, setErrorMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState('');
   const userInfo = useSelector((state: IUserInfoRootState) => {
     return state.userInfo.userInformation;
   });
   useEffect(() => {
     if (userInfo) {
-      router.push("/");
+      router.push('/');
     }
   }, [userInfo, router]);
   async function LoginHandler(user: IUser) {
     const { email, password } = user;
     try {
-      const { data } = await axios.post("/api/users/login", {
+      const { data } = await axios.post('/api/users/login', {
         email,
         password,
       });
       console.log(getError(data));
       dispatch(userInfoActions.userLogin(data));
-      jsCookie.set("userInfo", JSON.stringify(data));
-      router.push("/");
+      jsCookie.set('userInfo', JSON.stringify(data));
+      router.push('/');
     } catch (err: any) {
       setErrorMessage(getError(err));
       console.log(getError(err));
     }
   }
-  return (
-    <EnteringBox
-      title="login"
-      submitHandler={LoginHandler}
-      errorMessage={errorMessage}
-    />
-  );
+  return <EnteringBox title="login" submitHandler={LoginHandler} errorMessage={errorMessage} />;
 };
 
 export default Login;
